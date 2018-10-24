e0c21dba8ab8ecca83fb89a22776064108fb1d33
Ignat Vilesov <v-viig@microsoft.com>
2018-10-25 00:14:31 +0300

    Applies styles to xlsx

--- src/excelGenerator.js
+++ src/excelGenerator.js
@@ -16,7 +16,27 @@ class ExcelGenerator {
             'Description',
             'Started Date',
             'Completion Date',
-        ])
+        ]);
+
+        worksheet.getColumn(1).width = 24;
+        worksheet.getColumn(2).width = 12;
+        worksheet.getColumn(3).width = 78;
+        worksheet.getColumn(4).width = 16;
+        worksheet.getColumn(5).width = 16;
+
+        const headerRow = worksheet.lastRow;
+
+        headerRow.height = 25;
+
+        [
+            worksheet.getCell('A1'),
+            worksheet.getCell('B1'),
+            worksheet.getCell('C1'),
+            worksheet.getCell('D1'),
+            worksheet.getCell('E1'),
+        ].forEach((element) => {
+            this.applyHeaderStyles(element);
+        });
 
         for (let task of tasks) {
             worksheet.addRow([
@@ -26,9 +46,70 @@ class ExcelGenerator {
                 task.startDate,
                 task.endDate,
             ]);
+
+            worksheet.lastRow.font = {
+                name: 'Tahoma',
+                color: {
+                    argb: '00000000'
+                },
+                family: 2,
+                size: 10,
+            };
         }
 
         await workbook.xlsx.writeFile(outputFilePath);
+    }
+
+    applyHeaderStyles(element) {
+        element.font = {
+            name: 'Tahoma',
+            color: {
+                argb: '00FFFFFF'
+            },
+            family: 2,
+            size: 8,
+            bold: true,
+        };
+
+        element.alignment = {
+            vertical: 'middle',
+            horizontal: 'center'
+        };
+
+        element.fill = {
+            type: 'pattern',
+            pattern: 'solid',
+            fgColor: {
+                argb: '00130082'
+            }
+        };
+
+        element.border = {
+            top: {
+                style: 'thin',
+                color: {
+                    argb: '00FFFFFF'
+                }
+            },
+            left: {
+                style: 'thin',
+                color: {
+                    argb: '00FFFFFF'
+                }
+            },
+            bottom: {
+                style: 'thin',
+                color: {
+                    argb: '00FFFFFF'
+                }
+            },
+            right: {
+                style: 'thin',
+                color: {
+                    argb: '00FFFFFF'
+                }
+            }
+        };
     }
 }
 
